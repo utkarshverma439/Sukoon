63653dcf6a6794fca58a4da0fc1cd6289cafa4fe
Utkarsh Verma <129519314+utkarshverma439@users.noreply.github.com>
2025-10-21 21:41:24 +0530

    Use environment variable for uvicorn port

--- app.py
+++ app.py
@@ -347,4 +347,5 @@ async def clear_all_data(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
